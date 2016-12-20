a768b8bfb6ffa4be73489c8942a0bd2852b2e55d
sushiljainam <sushiljain1993@gmail.com>
2016-12-20 11:50:03 +0530

    after some modifications

---
+++ game-server/app/servers/connector/entryHandler1.js
@@ -0,0 +1,52 @@
+module.exports = function(app) {
+  return new Handler(app);
+};
+
+var Handler = function(app) {
+  this.app = app;
+};
+
+/**
+ * New client entry.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.entry = function(msg, session, next) {
+  console.log(msg);
+  next(null, {code: 200, msg: msg+'; game server isssssssssss ok.'});
+};
+
+/**
+ * Publish route for mqtt connector.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.publish = function(msg, session, next) {
+	var result = {
+		topic: 'publish',
+		payload: {code: 200, msg: 'publish message isssss ok.'}
+	};
+  next(null, result);
+};
+
+/**
+ * Subscribe route for mqtt connector.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next step callback
+ * @return {Void}
+ */
+Handler.prototype.subscribe = function(msg, session, next) {
+	var result = {
+		topic: 'subscribe',
+		payload: JSON.stringify({code: 200, msg: 'subscribe message is ok.'})
+	};
+  next(null, result);
+};
