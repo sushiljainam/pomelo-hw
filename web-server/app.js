cfe50b9f8382900448a2d83ffe49d8c0bcd92143
sushiljainam <sushiljain1993@gmail.com>
2016-12-27 11:56:21 +0530

    express mongo conn done

--- web-server/app.js
+++ web-server/app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var pub = require('../shared/infoPublisher');
+var MongoClient = require('mongodb').MongoClient
 
 var port = 7007;
-pub.bind('tcp://*:'+port, function(err) {
-    console.log('Listening for zmq subscribers  '+port+' ...');
-  });
+// pub.bind('tcp://*:'+port, function(err) {
+//     console.log('Listening for zmq subscribers  '+port+' ...');
+//   });
 
 var app = express.createServer();
 
@@ -42,5 +43,18 @@ app.use('/',function(req,res){
   res.json({info:453988765443,msg:"request rcvd at web server"});
 });
 
+MongoClient.connect('mongodb://localhost:26006/HWdb', function (err, db) {
+  if (err) {
+    console.log(err);
+    throw err}
+
+  db.collection('col1').find().toArray(function (err, result) {
+    if (err) {
+      console.log(err);
+      throw err}
+
+    console.log(result);
+  })
+})
 
 app.listen(3006);
